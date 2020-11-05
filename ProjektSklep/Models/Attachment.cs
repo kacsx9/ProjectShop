c0253c72d0ade06f96402e7ddb43ccc77ab27b78
Bartlomiej Uminski <bartlomiejuminski1999@gmail.com>
2020-11-05 22:11:53 +0100

    Dodane modele

---
+++ ProjektSklep/Models/Attachment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektSklep.Models
+{
+    public class Attachment
+    {
+        public int AttatchmentId { get; set; }
+
+        public string Path { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ ProjektSklep/Models/Expert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektSklep.Models
+{
+    public class Expert
+    {
+        public int ExpertId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
