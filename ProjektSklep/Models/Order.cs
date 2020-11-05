ad6dba556163a7fc3aafa8629b934040ec05c595
Kozik <michal.kozikowski99@gmail.com>
2020-11-05 22:33:00 +0100

    Dodano modele: Category, Order, Product.

---
+++ ProjektSklep/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektSklep.Models
+{
+    public enum State
+    {
+        Preparing = 0,
+        OnTheWay = 1,
+        Delivered = 2
+    }
+
+    public class Order
+    {
+        //pola
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public List<Product> OrderList { get; set; }
+        public int ShippingMethodID { get; set; }
+        public int PaymentMethod { get; set; }
+        public State OrderStatus { get; set; }
+
+        //metody
+        public bool SendOrderStatement()
+        {
+            return true;
+        }
+    }
+}
